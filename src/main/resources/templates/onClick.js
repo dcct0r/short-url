410ebed02aeb368953f147fbf8e3d97535bb9402
Roman <ckppaaa@gmail.com>
2023-09-16 22:35:49 +0300

    Clipboard added

--- src/main/resources/templates/onClick.js
+++ src/main/resources/templates/onClick.js
@@ -17,3 +17,18 @@ function viewField() {
         onAcceptButton.style.visibility="hidden";
     }
 }
+
+function copyToClipboard() {
+    // Get the text field
+    const copyText = document.getElementById("id_short_url_field");
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value).then(r => console.log("error"));
+
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+}
